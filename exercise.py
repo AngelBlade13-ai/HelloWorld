a5fd41479b559c3efef91bcbf9ec4d03a1f8a76f
AngelBlade13-ai <bladenmckinlay@gmail.com>
2026-03-24 15:44:55 -0600

    lesson compeletion

---
+++ exercise.py
@@ -0,0 +1,5 @@
+myName = "Bladen"
+myAge = 25
+
+print("My name is", myName)
+print("My age is", myAge)
---
+++ madlib.py
@@ -0,0 +1,5 @@
+noun = input("What is a noun: ")
+verb = input("Add a verb: ")
+plural_noun = input("Add a plural noun: ")
+
+print(f"The {noun} jumps over the fence. Then {verb} to the big red house, finally stopping to catch their breath, they invited {plural_noun} to the garage.")
